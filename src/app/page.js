324be901ff8cc4a0c0d924ee461e99df738516b1
tyVespA <migliavacca.marco08@gmail.com>
2024-12-30 23:27:16 +0100

    Create and style hero (basic)

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
+import cane_hero from "../../public/images/cane_hero.png";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint error
-        quibusdam inventore, vitae facere consequatur quia, minima voluptatum
-        explicabo officiis deserunt iusto quis ut facilis doloribus alias
-        delectus vero molestias!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos
-        voluptate quae. Ullam necessitatibus ab, dolores explicabo magnam
-        excepturi maiores eaque. Quo numquam quisquam excepturi explicabo quae
-        minus eos necessitatibus!
-      </p>
-      <h2>Heading 2</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
-        quas excepturi modi minus impedit. Eum quidem suscipit labore
-        reprehenderit officiis adipisci consequuntur eius molestias, corrupti
-        recusandae corporis, iste pariatur! Ex.
-      </p>
+      <section className={styles.heroContainer}>
+        <div className={styles.textContainer}>
+          <h1>Home</h1>
+          <h2>text</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+            velit vel officiis ut repellat ratione dicta? Et vitae neque, fuga
+            pariatur, ratione officiis tenetur eveniet odio eum nisi molestias
+            soluta!
+          </p>
+        </div>
+        <div className={styles.heroImgContainer}>
+          <Image src={cane_hero} alt="" />
+        </div>
+      </section>
+
+      <div className={styles.section2Container}>
+        <section className={styles.section2}>
+          <h2>section2</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint error
+            quibusdam inventore, vitae facere consequatur quia, minima
+            voluptatum explicabo officiis deserunt iusto quis ut facilis
+            doloribus alias delectus vero molestias!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos
+            voluptate quae. Ullam necessitatibus ab, dolores explicabo magnam
+            excepturi maiores eaque. Quo numquam quisquam excepturi explicabo
+            quae minus eos necessitatibus!
+          </p>
+          <h2>Heading 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+            quas excepturi modi minus impedit. Eum quidem suscipit labore
+            reprehenderit officiis adipisci consequuntur eius molestias,
+            corrupti recusandae corporis, iste pariatur! Ex.
+          </p>
+        </section>
+      </div>
     </div>
   );
 }
