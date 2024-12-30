cbb0999753a6f3327104d9c505707fd710da6856
tyVespA <migliavacca.marco08@gmail.com>
2024-12-30 11:19:52 +0100

    Add icons, fix layout

--- src/app/components/PreviewCard.js
+++ src/app/components/PreviewCard.js
@@ -1,6 +1,7 @@
 import styles from "./PreviewCard.module.css";
 import Image from "next/image";
 import { calculateAge } from "../../../utils/calculateAge";
+import { GenderMale, GenderFemale, Cake, Ruler } from "@phosphor-icons/react";
 
 export default function PreviewCard({ item }) {
   const anno_di_nascita = item.anno_di_nascita;
@@ -12,14 +13,28 @@ export default function PreviewCard({ item }) {
     <div className={styles.previewCardContainer}>
       <div>
         <div className={styles.imageContainer}>
-          <Image src={item.img1} width={3142} height={3927} alt="" />
+          <Image src={item.img1} width={3142 / 10} height={3927 / 10} alt="" />
         </div>
         <div className={styles.textContainer}>
           <p className={styles.nome}>{item.nome}</p>
-          <p>
-            {item.sesso}, {età}
-          </p>
-          <p className={styles.taglia}>Taglia {item.taglia}</p>
+          <div className={styles.stats}>
+            <p className={styles.sesso}>
+              {item.sesso.toLowerCase() === "maschio" ? (
+                <GenderMale size={18} />
+              ) : (
+                <GenderFemale size={18} />
+              )}
+              {item.sesso}
+            </p>
+            <p className={styles.età}>
+              {" "}
+              <Cake size={18} />
+              {età}
+            </p>
+            <p className={styles.taglia}>
+              <Ruler size={18} /> Taglia {item.taglia}
+            </p>
+          </div>
         </div>
       </div>
     </div>
