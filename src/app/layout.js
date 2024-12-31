c06eb40c0619f63375743e09933af5e6dc8d7a36
tyVespA <migliavacca.marco08@gmail.com>
2024-12-31 16:36:40 +0100

    test openGraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,14 @@ export const metadata = {
       },
     ],
   },
+  openGraph: {
+    images: {
+      url: "https://cdn.7tv.app/emote/01HKWA1EB00005BZE9KQ0QYJW7/4x.avif",
+      width: 200,
+      height: 200,
+      alt: "Canile di Macondo",
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
