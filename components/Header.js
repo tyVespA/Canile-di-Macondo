f6e31a09b82c3017d75cafb6f3c9043cc476f150
tyVespA <migliavacca.marco08@gmail.com>
2025-01-02 12:41:48 +0100

    Add styles on scroll

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Dog } from "@phosphor-icons/react/dist/ssr";
 import styles from "./Header.module.css";
@@ -5,18 +7,34 @@ import Navbar from "./Navbar";
 import Button from "./Button";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <Link href="/">
-          <Dog size={35} weight="duotone" />
-        </Link>
-      </div>
-      <Navbar />
-      <div className={styles.ctaButtons}>
-        <Button theme="dark">Dona</Button>
-        <Button theme="light">Adotta</Button>
-      </div>
-    </header>
+    <div className={styles.stickyHeaderContainer}>
+      <header
+        className={`${styles.header} max-width-container ${
+          scrolled ? styles.scrolled : ""
+        }`}
+      >
+        <div className={styles.logoContainer}>
+          <Link href="/">
+            <Dog size={35} weight="duotone" />
+          </Link>
+        </div>
+        <Navbar />
+        <div className={styles.ctaButtons}>
+          <Button theme="dark">Dona</Button>
+          <Button theme="light">Adotta</Button>
+        </div>
+      </header>
+    </div>
   );
 }
