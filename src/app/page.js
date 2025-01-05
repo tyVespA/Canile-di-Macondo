cc566fb742e77e14040b3ffd587eeda20fd069ca
tyVespA <migliavacca.marco08@gmail.com>
2025-01-05 17:48:09 +0100

    Fix bg

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import styles from "./page.module.css";
 import SwitchVersionButton from "./shared-components/SwitchVersionButton";
 import { Dog } from "@phosphor-icons/react/dist/ssr";
+import FullWidthBg from "./shared-components/FullWidthBg";
 
 export const metadata = {
   title: "Home | Canile di Macondo",
@@ -11,15 +12,17 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className={styles.pageContainer}>
-      <div className={styles.title}>
-        <Dog size={75} weight="fill" />
-        <h1>Benvenuti al canile di Macondo :)</h1>
+    <FullWidthBg backgroundColor="hsl(248, 97%, 64%)">
+      <div className={styles.pageContainer}>
+        <div className={styles.title}>
+          <Dog size={75} weight="fill" />
+          <h1>Benvenuti al canile di Macondo :)</h1>
+        </div>
+        <div className={styles.buttonsContainer}>
+          <Link href="/moderna">Versione colorata</Link>
+          <Link href="/tradizionale">Versione chiara</Link>
+        </div>
       </div>
-      <div className={styles.buttonsContainer}>
-        <Link href="/moderna">Versione colorata</Link>
-        <Link href="/tradizionale">Versione chiara</Link>
-      </div>
-    </div>
+    </FullWidthBg>
   );
 }
