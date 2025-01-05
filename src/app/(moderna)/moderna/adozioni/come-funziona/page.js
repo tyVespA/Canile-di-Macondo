72a21075bc7d3f3d79aadf15e9b103c9353c5170
tyVespA <migliavacca.marco08@gmail.com>
2025-01-05 16:37:04 +0100

    Add content

--- src/app/(moderna)/moderna/adozioni/come-funziona/page.js
+++ src/app/(moderna)/moderna/adozioni/come-funziona/page.js
@@ -1,3 +1,5 @@
+import styles from "./page.module.css";
+
 export const metadata = {
   title: "Come funziona | Canile di Macondo",
   description: "Benvenuti!" /* change */,
@@ -5,8 +7,44 @@ export const metadata = {
 
 export default function page() {
   return (
-    <div>
-      <h1>come funziona</h1>
+    <div className={styles.comeFunzionaPage}>
+      <section className="first-section">
+        <h1>Il processo di adozione</h1>
+        <h2>
+          Adopting a new cat or dog is like a marriage. <br /> We want it to be
+          a life-long, successful partnership.
+        </h2>
+        <p>
+          To ensure its success we need to know as much as possible about each
+          individual/family’s needs and lifestyle, so that we can recommend the
+          best possible match.
+        </p>
+        <h2>Important Points to Consider:</h2>
+        <ol>
+          <li>
+            Experience and knowledge of dogs/cats, their behaviour, needs,
+            habits, comforts, food, health and exercise;
+          </li>
+          <li>
+            Your family circumstances: <br /> Do you have enough space, time and
+            money to care for a dog/cat?
+          </li>
+          <li>
+            Housing conditions: <br /> Are you allowed to keep a pet? Is there
+            likely to be trouble with neighbours?
+          </li>
+          <li>
+            On average, how long are you away from home each day/week? Some
+            animals left all day alone get stressed out from loneliness;
+          </li>
+          <li>
+            Are you able to make a long-term commitment to this dog/cat?
+            Dogs/cats live an average of 18 years;
+          </li>
+          <li>If you already have a pet, is it neutered?</li>
+        </ol>
+        <h2>The Adoption Application Procedure</h2>
+      </section>
     </div>
   );
 }
