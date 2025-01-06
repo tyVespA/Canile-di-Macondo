fd6b80d069ca61c5baf34e85372ef68ece47ad21
tyVespA <migliavacca.marco08@gmail.com>
2025-01-06 19:22:01 +0100

    Fix import

--- src/app/(tradizionale)/tradizionale/come-aiutarci/page.js
+++ src/app/(tradizionale)/tradizionale/come-aiutarci/page.js
@@ -1,6 +1,5 @@
 import style from "./page.module.css";
 import Image from "next/image";
-import shiba from "@images/vecteezy_shiba-inu-2.png";
 import TitleBanner from "@shared-components/TitleBanner";
 import testImage from "@images/tradizionale/hero_image.jpg";
 
