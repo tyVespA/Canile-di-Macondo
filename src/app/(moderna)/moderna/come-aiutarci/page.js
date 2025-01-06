1095b249d227dd78f9ee8d4050adb0839c625da9
tyVespA <migliavacca.marco08@gmail.com>
2025-01-06 19:16:35 +0100

    Fix import

--- src/app/(moderna)/moderna/come-aiutarci/page.js
+++ src/app/(moderna)/moderna/come-aiutarci/page.js
@@ -1,6 +1,6 @@
 import style from "./page.module.css";
 import Image from "next/image";
-import shiba from "@images/vecteezy_shiba-inu-2.png";
+// import shiba from "@images/vecteezy_shiba-inu-2.png";
 
 export default function page() {
   return (
@@ -14,7 +14,7 @@ export default function page() {
       // className="first-section"
     >
       <h1>Come aiutarci</h1>
-      <Image src={shiba} alt="" height={150} />
+      {/* <Image src={shiba} alt="" height={150} /> */}
     </div>
   );
 }
