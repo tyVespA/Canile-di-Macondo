b548122f045084a0c8a66f01b7e471cf994621ed
tyVespA <migliavacca.marco08@gmail.com>
2025-01-07 20:44:28 +0100

    Add props, tweak styles

--- src/app/(moderna)/moderna/adozioni/i-nostri-ospiti/page.js
+++ src/app/(moderna)/moderna/adozioni/i-nostri-ospiti/page.js
@@ -2,7 +2,8 @@
 import { useState } from "react";
 import styles from "./page.module.css";
 import { db } from "@lib/db";
-import PreviewCardsSection from "../../components/PreviewCardsSection";
+// import PreviewCardsSection from "../../components/PreviewCardsSection";
+import PreviewCardsSection from "@shared-components/PreviewCardsSection";
 
 export default function page() {
   const [filters, setFilters] = useState({
@@ -18,14 +19,15 @@ export default function page() {
     return true;
   });
   return (
-    <div>
+    <div className={styles.ospitiPage}>
       <title>I nostri ospiti | Canile di Macondo</title>
       <meta
         name="description"
         content="Scopri i nostri ospiti, i cani in attesa di adozione presso il Canile di Macondo."
       />
       <section className="first-section">
-        <h1>i nostri ospiti</h1>
+        <h1>I nostri ospiti</h1>
+        <p>inserire warning card</p>
         <div className={styles.filters}>
           <select
             value={filters.sesso}
@@ -44,12 +46,28 @@ export default function page() {
             <option value="media">media</option>
             <option value="grande">grande</option>
           </select>
+          <select
+            value={filters.taglia}
+            onChange={(e) => setFilters({ ...filters, taglia: e.target.value })}
+          >
+            <option value="all">All età</option>
+            <option value="piccola">piccola</option>
+            <option value="media">media</option>
+            <option value="grande">grande</option>
+          </select>
         </div>
         <div>
           {filteredData.length > 0 ? (
-            <PreviewCardsSection data={filteredData} />
+            <PreviewCardsSection
+              data={filteredData}
+              backgroundColor="var(--accent-one)"
+              color="var(--text-dark)"
+              versione="moderna"
+            />
           ) : (
-            <p>No data</p>
+            <p style={{ textAlign: "center", marginBottom: "25px" }}>
+              Al momento non abbiamo ospiti con queste caratteristiche!
+            </p>
           )}
         </div>
       </section>
--- src/app/(tradizionale)/tradizionale/adozioni/i-nostri-ospiti/page.js
+++ src/app/(tradizionale)/tradizionale/adozioni/i-nostri-ospiti/page.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import styles from "./page.module.css";
 import { db } from "@lib/db";
-import PreviewCardsSection from "../../components/PreviewCardsSection";
+import PreviewCardsSection from "@shared-components/PreviewCardsSection";
 
 export default function page() {
   const [filters, setFilters] = useState({
@@ -17,40 +17,58 @@ export default function page() {
     return true;
   });
   return (
-    <div>
+    <div className={styles.ospitiPage}>
       <title>I nostri ospiti | Canile di Macondo</title>
       <meta
         name="description"
         content="Scopri i nostri ospiti, i cani in attesa di adozione presso il Canile di Macondo."
       />
+      <section className="first-section">
+        <h1>i nostri ospiti</h1>
+        <div className={styles.filters}>
+          <select
+            value={filters.sesso}
+            onChange={(e) => setFilters({ ...filters, sesso: e.target.value })}
+          >
+            <option value="all">All sesso</option>
+            <option value="Maschio">Maschio</option>
+            <option value="Femmina">Femmina</option>
+          </select>
+          <select
+            value={filters.taglia}
+            onChange={(e) => setFilters({ ...filters, taglia: e.target.value })}
+          >
+            <option value="all">All taglia</option>
+            <option value="piccola">piccola</option>
+            <option value="media">media</option>
+            <option value="grande">grande</option>
+          </select>
+          <select
+            value={filters.taglia}
+            onChange={(e) => setFilters({ ...filters, taglia: e.target.value })}
+          >
+            <option value="all">All età</option>
+            <option value="piccola">piccola</option>
+            <option value="media">media</option>
+            <option value="grande">grande</option>
+          </select>
+        </div>
 
-      <h1>i nostri ospiti</h1>
-      <div className={styles.filters}>
-        <select
-          value={filters.sesso}
-          onChange={(e) => setFilters({ ...filters, sesso: e.target.value })}
-        >
-          <option value="all">All sesso</option>
-          <option value="Maschio">Maschio</option>
-          <option value="Femmina">Femmina</option>
-        </select>
-        <select
-          value={filters.taglia}
-          onChange={(e) => setFilters({ ...filters, taglia: e.target.value })}
-        >
-          <option value="all">All taglia</option>
-          <option value="piccola">piccola</option>
-          <option value="media">media</option>
-          <option value="grande">grande</option>
-        </select>
-      </div>
-      <div>
-        {filteredData.length > 0 ? (
-          <PreviewCardsSection data={filteredData} />
-        ) : (
-          <p>No data</p>
-        )}
-      </div>
+        <div>
+          {filteredData.length > 0 ? (
+            <PreviewCardsSection
+              data={filteredData}
+              backgroundColor="var(--accent-three)" /* to change */
+              color="var(--text-light)"
+              versione="tradizionale"
+            />
+          ) : (
+            <p style={{ textAlign: "center", marginBottom: "25px" }}>
+              Al momento non abbiamo ospiti con queste caratteristiche!
+            </p>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
