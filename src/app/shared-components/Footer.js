abbe1907499d8b7b24eac5bb15d3fcf2c4cd44b8
tyVespA <migliavacca.marco08@gmail.com>
2025-01-11 10:35:58 +0100

    Add socials icons

--- src/app/shared-components/Footer.js
+++ src/app/shared-components/Footer.js
@@ -1,12 +1,32 @@
 import styles from "./Footer.module.css";
 import Link from "next/link";
 import FullWidthBg from "./FullWidthBg";
+import {
+  FacebookLogo,
+  InstagramLogo,
+  YoutubeLogo,
+  TiktokLogo,
+} from "@phosphor-icons/react/dist/ssr";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className={styles.footer}>
+      <div className={styles.socialsContainer}>
+        <Link href="/">
+          <FacebookLogo size={32} />
+        </Link>
+        <Link href="/">
+          <InstagramLogo size={32} />
+        </Link>
+        <Link href="/">
+          <YoutubeLogo size={32} />
+        </Link>
+        <Link href="/">
+          <TiktokLogo size={32} />
+        </Link>
+      </div>
       <p className={styles.copyright}>
         Sito sviluppato da ©{" "}
         <Link
