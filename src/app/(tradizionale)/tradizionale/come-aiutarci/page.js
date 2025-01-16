0ea3c60947cd277b96956470b32e3b4af343201b
tyVespA <migliavacca.marco08@gmail.com>
2025-01-16 15:07:57 +0100

    Add content and style

--- src/app/(tradizionale)/tradizionale/come-aiutarci/page.js
+++ src/app/(tradizionale)/tradizionale/come-aiutarci/page.js
@@ -1,31 +1,27 @@
-import style from "./page.module.css";
+import styles from "./page.module.css";
 import Image from "next/image";
 import TitleBanner from "@shared-components/TitleBanner";
 import testImage from "@images/tradizionale/hero_image.jpg";
 import Link from "next/link";
 
+import qrCode from "@images/Codice_QR.png";
+
+import PaypalCampagna from "@/src/app/shared-components/PaypalCampagna";
 import PaypalButton from "@/src/app/shared-components/PaypalButton";
 
 export default function page() {
   return (
     <div>
       <TitleBanner imgSrc={testImage}>Come aiutarci</TitleBanner>
-      <div>
-        <iframe
-          src="https://www.paypal.com/giving/campaigns?campaign_id=WVPPC226FLLEC"
-          title="PayPal donate campaign card"
-          frameBorder="0"
-          width={382}
-          height={420}
-          scrolling="no"
-        ></iframe>
+      <div className={styles.campagnaContainer}>
+        <PaypalCampagna />
       </div>
-      <Link href="https://www.paypal.com/donate/?hosted_button_id=5MWVZ2KT58Y9E">
-        {/* <button>test</button> */}
-      </Link>
-      <br />
-      <PaypalButton width="200px" />
-      <br />
+      <div className={styles.buttonContainer}>
+        <PaypalButton width="200px" />
+      </div>
+      <div className={styles.qrCodeContainer}>
+        <Image src={qrCode} alt="codice QR"></Image>
+      </div>
     </div>
   );
 }
