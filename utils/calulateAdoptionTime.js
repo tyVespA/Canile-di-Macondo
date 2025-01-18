90126123ed7d859c4d41b22a49067f9b5049b37a
tyVespA <migliavacca.marco08@gmail.com>
2025-01-18 16:14:50 +0100

    fix edge cases, add docum

--- utils/calulateAdoptionTime.js
+++ utils/calulateAdoptionTime.js
@@ -5,26 +5,38 @@ export function calculateAdoptionTime(adozione_anno, adozione_mese) {
   const mese_corrente = new Date().getMonth() + 1;
   let adottatoDa;
 
+  // adottato da meno di un anno
   if (anno_corrente === adozione_anno && mese_corrente != adozione_mese) {
     if (12 - adozione_mese + mese_corrente === 1) {
-      adottatoDa = `1 mese`;
+      return `un mese`;
     } else {
-      adottatoDa = `${mese_corrente - adozione_mese} mesi`;
+      return `${mese_corrente - adozione_mese} mesi`;
     }
-  } else if (anno_corrente === adozione_anno + 1) {
+  }
+  // adottato da meno di un anno ma con anno di adozione diverso da anno corrente
+  else if (
+    anno_corrente === adozione_anno + 1 &&
+    12 - adozione_mese + mese_corrente != 12
+  ) {
     if (12 - adozione_mese + mese_corrente === 1) {
-      adottatoDa = `1 mese`;
+      return `un mese`;
     } else {
-      adottatoDa = `${12 - adozione_mese + mese_corrente} mesi`;
+      return `${12 - adozione_mese + mese_corrente} mesi`;
     }
-  } else if (
-    mese_corrente === adozione_mese &&
+  }
+  // adottato da meno di un mese
+  else if (mese_corrente === adozione_mese && anno_corrente === adozione_anno) {
+    return "qualche giorno";
+  }
+  // adottato da esattamente un anno
+  else if (
+    12 - adozione_mese + mese_corrente == 12 &&
     anno_corrente === adozione_anno
   ) {
-    adottatoDa = "qualche giorno";
-  } else {
-    adottatoDa = `${anno_corrente - adozione_anno} anni`;
+    return "un anno";
   }
-
-  return adottatoDa;
+  // adottato da più di un anno
+  else {
+    return `${anno_corrente - adozione_anno} anni`;
+  }
 }
