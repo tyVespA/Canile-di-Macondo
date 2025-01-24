18f2cf3479614aa95ccf9c213e9e2e117ae41717
tyVespA <migliavacca.marco08@gmail.com>
2025-01-24 14:00:34 +0100

    Change marginBlock as prop

--- src/app/shared-components/Spacer.js
+++ src/app/shared-components/Spacer.js
@@ -1,9 +1,9 @@
 import styles from "./Spacer.module.css";
 import { PawPrint } from "@phosphor-icons/react/dist/ssr";
 
-export default function Spacer() {
+export default function Spacer({ marginBlock = "75px" }) {
   return (
-    <div className={styles.spacer}>
+    <div className={styles.spacer} style={{ marginBlock }}>
       <PawPrint size={24} weight="fill" fill="#9c6644" />
       <PawPrint size={24} weight="fill" fill="#9c6644" />
       <PawPrint size={24} weight="fill" fill="#9c6644" />
