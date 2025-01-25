124b8994a69db6005c5592053e3eecc6adc13301
tyVespA <migliavacca.marco08@gmail.com>
2025-01-25 18:34:29 +0100

    Create and style

--- src/app/shared-components/Testimonial.js
+++ src/app/shared-components/Testimonial.js
@@ -2,9 +2,11 @@ import styles from "./Testimonial.module.css";
 
 export default function Testimonial({ name, children }) {
   return (
-    <div>
-      <p>{children}</p>
-      <h4>{name}</h4>
+    <div className={styles.testimonialContainer}>
+      <div className={styles.testimonialContent}>
+        <h4>{name}</h4>
+        <p>{children}</p>
+      </div>
     </div>
   );
 }
