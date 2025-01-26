b9ce7e31f9492fbdc48ab4cf1f7ed6c7ae66d0f7
tyVespA <migliavacca.marco08@gmail.com>
2025-01-26 11:22:27 +0100

    Add image prop and style

--- src/app/shared-components/Testimonial.js
+++ src/app/shared-components/Testimonial.js
@@ -1,10 +1,18 @@
 import styles from "./Testimonial.module.css";
+import Image from "next/image";
 
-export default function Testimonial({ name, children }) {
+export default function Testimonial({ name, ospite, children }) {
+  const lowerCaseOspite = ospite.toLowerCase();
   return (
     <div className={styles.testimonialContainer}>
       <div className={styles.testimonialContent}>
         <h4>{name}</h4>
+        <Image
+          src={`/images/adottati/${lowerCaseOspite}/${lowerCaseOspite}1.jpg`}
+          alt={`foto di ${ospite}`}
+          width={150}
+          height={150}
+        />
         <p>{children}</p>
       </div>
     </div>
